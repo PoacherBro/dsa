dae82b3fe779bf5241477a6a136dfb0d9d87dbfa
Leo Liu <liuxin19900213@gmail.com>
2017-02-19 11:06:17 +0800

    MergeSort - 归并排序的迭代版实现

--- src/main/java/algorithms/MergeSort.java
+++ src/main/java/algorithms/MergeSort.java
@@ -1,6 +1,12 @@
 package algorithms;
 
 /**
+ * 归并排序是建立在归并操作上的排序，该算法采用分治法
+ * 1. 把需要排序的数组依次划分为最小单元（1或者2个元素）
+ * 2. 再把每个子单元依次合并
+ *
+ * 具体可参考：https://zh.wikipedia.org/wiki/%E5%BD%92%E5%B9%B6%E6%8E%92%E5%BA%8F
+ *
  * @author Leo on 2017/2/14.
  */
 public class MergeSort {
@@ -45,14 +51,49 @@ public class MergeSort {
      * @param arr 要排序的数组
      */
     private static void mergeSort(int[] arr) {
-        //TODO
+        int length = arr.length;
+        if (length <= 1) {
+            return;
+        }
+
+        int[] result = new int[length];
+        int block, start;
+
+        // 最小的块为1，然后是block * 2的块合并
+        for (block = 1; block < length * 2; block *= 2) {
+            // 把数组按照块划分，然后合并
+            for (start = 0; start < length; start += 2 * block) {
+                int low = start;
+                int mid = (start + block) < length ? (start + block) : length;
+                int high = (start + 2 * block) < length ? (start + 2 * block) : length;
+
+                // 两个块的起始位置以及结束位置
+                int start1 = low, end1 = mid;
+                int start2 = mid, end2 = high;
+
+                while (start1 < end1 && start2 < end2) {
+                    result[low++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+                }
+                while (start1 < end1) {
+                    result[low++] = arr[start1++];
+                }
+                while (start2 < end2) {
+                    result[low++] = arr[start2++];
+                }
+            }
+
+            int[] temp = arr;
+            arr = result;
+            result = temp;
+        }
     }
 
 
     public static void main(String[] args) {
         int[] arr = {5, 8, 3, 4, 9, 7};
         int[] result = new int[arr.length];
-        mergeSortRecursive(arr, result, 0, arr.length - 1);
+//        mergeSortRecursive(arr, result, 0, arr.length - 1);
+        mergeSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
