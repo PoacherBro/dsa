214a1c5419c7d6f3c9fb00b002e2293e65774900
Leo Liu <liuxin19900213@gmail.com>
2018-06-07 20:11:55 +0800

    对最长不重复子串添加问题描述

--- src/main/java/leetcode/LongestSubstring.java
+++ src/main/java/leetcode/LongestSubstring.java
@@ -8,6 +8,18 @@ import java.util.Set;
 
 /**
  * @author Leo on 2017/1/30
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ *
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ *
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ *
+ * Given "pwwkew", the answer is "wke", with the length of 3.
+ *
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 public class LongestSubstring {
     /**
