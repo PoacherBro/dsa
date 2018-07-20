8a842acba7089aba73748e0af48d3916724f2856
Leo Liu <liuxin19900213@gmail.com>
2018-07-20 10:46:34 +0800

    add data-structure: binary search tree (not finished)

---
+++ src/main/java/ds/BinarySearchTree.java
@@ -0,0 +1,13 @@
+package ds;
+
+public class BinarySearchTree<T extends Comparable> {
+    private class Node {
+        boolean isRoot;
+        Node lef;
+        Node right;
+    }
+
+    public void <T> add(T value) {
+    }
+    
+}
