7ac01541636f4b31339b6af20e156a16920a7817
Leo Liu <liuxin19900213@gmail.com>
2020-03-28 20:08:01 +0800

    fix import uncorrect path

--- algs/binarysearch/binary_search_test.go
+++ algs/binarysearch/binary_search_test.go
@@ -3,12 +3,12 @@ package binarysearch_test
 import (
 	"testing"
 
-	"github.com/PoacherBro/dsa/algs/binary_search"
+	"github.com/PoacherBro/dsa/algs/binarysearch"
 )
 
 func TestBinarySearch(t *testing.T) {
 	a := []int{1, 45, 90, 100, 291, 311}
-	ret := binary_search.Search(45, a)
+	ret := binarysearch.Search(45, a)
 	if ret != 1 {
 		t.Error("test failed, not found matched key")
 	}
